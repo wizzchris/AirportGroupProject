c38102e2135ddfe84b9099b760383bf92d4a8fad
James Murphy <jmurphy@spartaglobal.com>
2020-02-12 12:23:40 +0000

    a little tidy up

--- PeopleClass.py
+++ PeopleClass.py
@@ -1,30 +1,29 @@
-class People:     # people class
-    def __init__(self, name):   # people class has defined attribute name
+class People:  # people class
+    def __init__(self, name):  # people class has defined attribute name
         self.name = name
 
 
-class Passenger(People):        # passenger subclass of people
+class Passenger(People):  # passenger subclass of people
     __passenger_id = 0
-
     def __init__(self, __passenger_id, name):
-        super().__init__(name)      # super to give subclass inheritance to main class
+        super().__init__(name)  # super to give subclass inheritance to main class
         self.__passport = ''
-        self.__passenger_id += 1    # adds 1 to the defined passenger id
-        Passenger.__passenger_id += 1   # continuously adds 1 to each new instance of a new passenger
+        self.__passenger_id += 1  # adds 1 to the defined passenger id
+        Passenger.__passenger_id += 1  # continuously adds 1 to each new instance of a new passenger
 
-    def add_passport(self,passportnum): # Adds a way to add a passport to the person
+    def add_passport(self, passportnum):  # Adds a way to add a passport to the person
         self.__passport = passportnum
         return self.__passport
 
-    def check_passport(self): # Returns the passport number
+    def check_passport(self):  # Returns the passport number
         return self.__passport
 
-    def add_passenger(self, passengernum): # adds a way to add a passenger to the person
+    def add_passenger(self, passengernum):  # adds a way to add a passenger to the person
         self.__passenger_id = passengernum
         return passengernum
 
-    def check_passport(self): # Returns the passport number
+    def check_passport(self):  # Returns the passport number
         return self.__passport
 
-    def check_passenger(self): # returns the passenger id
+    def check_passenger(self):  # returns the passenger id
         return self.__passenger_id
--- PlaneClass.py
+++ PlaneClass.py
@@ -10,11 +10,8 @@ class Plane:
 class Plane:
     Plane_id = 0  # Starts the id convention
 
-
-
     def __init__(self, capacity, manufacturer, model, flights=None, taken='no'):  # Initialises class with capacity
         self.capacity = capacity
-
         self.plane_id = 'A-P-' + str(Plane.Plane_id)  # Adds the plane id
         Plane.Plane_id += 1  # Adds 1 to the id after creation
         self.Airline = ''  # Allows the plane class to have airline
@@ -22,7 +19,6 @@ class Plane:
             flights = []
         self.flights = []
         self.taken = taken  # Check to see if plane is taken on a flight
-
         self.plane_id = 'A-P-' + str(Plane.Plane_id) #Adds the plane id
         Plane.Plane_id += 1  #Adds 1 to the id after creation
         self.Airline = ''  #Allows the plane class to have airline
