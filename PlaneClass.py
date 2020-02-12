daafc2ae6c14f7692eddd27cdb28ef495e8f4ecf
James Murphy <jmurphy@spartaglobal.com>
2020-02-12 11:54:59 +0000

    formatting of files

--- PlaneClass.py
+++ PlaneClass.py
@@ -1,19 +1,17 @@
 """
 Adds a class called plane
 """
+
 class Plane:
-    Plane_id = 0  #Starts the id convention
+    Plane_id = 0  # Starts the id convention
 
-
-    def __init__(self,capacity, flights=None, taken='no'):  # Initialises class with capacity
-
+    def __init__(self, capacity, flights=None, taken='no'):  # Initialises class with capacity
 
         self.capacity = capacity
-        self.plane_id = 'A-P-' + str(Plane.Plane_id) #Adds the plane id
-        Plane.Plane_id += 1  #Adds 1 to the id after creation
-        self.Airline = ''  #Allows the plane class to have airline
-        if flights == None:
+        self.plane_id = 'A-P-' + str(Plane.Plane_id)  # Adds the plane id
+        Plane.Plane_id += 1  # Adds 1 to the id after creation
+        self.Airline = ''  # Allows the plane class to have airline
+        if flights is None:
             flights = []
         self.flights = []
-        self.taken = taken #Check to see if plane is taken on a flight
-
+        self.taken = taken  # Check to see if plane is taken on a flight
