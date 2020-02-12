864f66a3b7562d071468e7dd17ea5f5e8cad8a8d
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-12 12:25:37 +0000

    updates made to work with Abdis code

--- RUN.py
+++ RUN.py
@@ -3,14 +3,17 @@ from PeopleClass import People, Passenger
 from PlaneClass import Plane
 
 plane_database = []
-
+passenger_database = []
 
 flight_class_instance = Flight('100', 'Virgin', 'Bangaladesh', '17/02/2020')
 
-
-# As a senior member of the airport staff, i want to register a plane.
+# 1.) As a senior member of the airport staff, i want to register a plane.
 flight_class_instance.add_plane('100000')
 
-plane_database.append(flight_class_instance)
+# TEST
+# plane_database.append(flight_class_instance)
+# print(plane_database[0].destination)
 
-print(plane_database[0].destination)
+
+passenger_database.append(passenger_1_instance)
+print(passenger_database[0].name)
