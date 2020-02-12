099117d09fdadcb267c315d75759c9078d603411
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-12 10:35:25 +0000

    Start

--- FlightClass.py
+++ FlightClass.py
@@ -1,15 +1,20 @@
+""""
+Adds a class called flight
+"""""
 
 class Flight:
-    def __init__(self,flight_num, airline, destination, date_time, boarding_list=[], origin='London'):
+    def __init__(self,flight_num, airline, destination, date_time, boarding_list= None, origin='London'):  #Starts class with flight number, airline, destination and date time. Allows for future creation of boarding list and origin
         self.flight_num = flight_num
         self.airline = airline
         self.destination = destination
         self.date_time = date_time
+        if boarding_list == None:  #Allows change to empty list if boarding list is empty
+            boarding_list = []
         self.boarding_list = boarding_list
         self.origin = origin
 
-    def add_customers(self,customer_list):
+    def add_customers(self,customer_list):  #Starts a class method which adds multiple customers as a list
         self.boarding_list.extend(customer_list)
 
-    def add_a_customer(self,customer):
+    def add_a_customer(self,customer):  #Adds a customer to the boarding list, only one customer
         self.boarding_list.append(customer)
--- PlaneClass.py
+++ PlaneClass.py
@@ -1,11 +1,12 @@
-
-
+"""
+Adds a class called plane
+"""
 class Plane:
-    Plane_id = 0
+    Plane_id = 0  #Starts the id convention
 
-    def __init__(self,capacity):
+    def __init__(self,capacity):  # Initialises class with capacity
 
         self.capacity = capacity
-        self.plane_id = 'A-P-' + str(Plane.Plane_id)
-        Plane.Plane_id += 1
-        self.Airline = ''
+        self.plane_id = 'A-P-' + str(Plane.Plane_id) #Adds the plane id
+        Plane.Plane_id += 1  #Adds 1 to the id after creation
+        self.Airline = ''  #Allows the plane class to have airline
