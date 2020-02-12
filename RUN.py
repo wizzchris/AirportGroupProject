2226662599605d6003ddb37d6422debde888fcd0
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-12 11:51:05 +0000

    updates made by T

--- RUN.py
+++ RUN.py
@@ -1,2 +1,9 @@
+from FlightClass import Flight
+from PeopleClass import People, Passenger
+from PlaneClass import Plane
+
 # As a senior member of the airport staff, i want to register a plane.
 
+flight_class_instance = Flight()
+
+flight_class_instance.add_plane('Airbus A220')
