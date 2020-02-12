ab8024bca20f828bfd378be26427718c20725bce
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-12 12:20:24 +0000

    added stuff to run

--- PeopleClass.py
+++ PeopleClass.py
@@ -12,10 +12,21 @@ class Passenger(People):        # passenger subclass of people
         self.__passenger_id += 1    # adds 1 to the defined passenger id
         Passenger.__passenger_id += 1   # continuously adds 1 to each new instance of a new passenger
 
-    def add_passport(self,passportnum): # Adds a way to add a passport to the person
+    def add_passport(self,passportnum): # adds a way to add a passport to the person
         self.__passport = passportnum
         return self.__passport
+
+    def add_passenger(self, passengernum): # adds a way to add a passenger to the person
+        self.__passenger_id = passengernum
+        return passengernum
 
     def check_passport(self): # Returns the passport number
         return self.__passport
 
+<<<<<<< HEAD
+=======
+    def check_passenger(self): # returns the passenger id
+        return self.__passenger_id
+
+
+>>>>>>> 674a9024fcd94ea0530b648e7d8260f056be190b
--- RUN.py
+++ RUN.py
@@ -1,4 +1,15 @@
+from FlightClass import Flight
+from PeopleClass import People, Passenger
+from PlaneClass import Plane
+
+plane_database = []
+
+
+flight_class_instance = Flight('100', 'Virgin', 'Bangaladesh', '17/02/2020')
+
+
 # As a senior member of the airport staff, i want to register a plane.
+<<<<<<< HEAD
 import PlaneClass
 import PeopleClass
 import FlightClass
@@ -33,3 +44,10 @@ while True:
 
     else:
         print('Please choose a valid command')
+=======
+flight_class_instance.add_plane('100000')
+
+plane_database.append(flight_class_instance)
+
+print(plane_database[0].destination)
+>>>>>>> 674a9024fcd94ea0530b648e7d8260f056be190b
