515c028ffa9f6feacfe91c14bf721786826b8390
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-13 11:45:07 +0000

    updates made

--- PeopleClass.py
+++ PeopleClass.py
@@ -52,3 +52,4 @@ class Passenger(People):  # passenger subclass of people
         for flight in flight_list:
             if self.destination == flight.destination:
                 flight.boarding_list.add_a_customer(self)
+# change
