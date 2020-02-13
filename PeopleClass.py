c1f9d5cfad9e51adba50574c0d299d54767809a1
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-13 11:07:38 +0000

    Corrected pep

--- PeopleClass.py
+++ PeopleClass.py
@@ -3,44 +3,32 @@ class People:  # people class
         self.name = name
 
 
-class Passenger(People):        # passenger subclass of people
+class Passenger(People):  # passenger subclass of people
 
     __passenger_id = 0
 
-    def __init__(self,name, destination):
-        super().__init__(name)      # super to give subclass inheritance to main class
+    def __init__(self, name, destination):
+        super().__init__(name)  # super to give subclass inheritance to main class
         self.__passport = ''
-        self.__passenger_id += 1    # adds 1 to the defined passenger id
-        Passenger.__passenger_id += 1   # continuously adds 1 to each new instance of a new passenger
+        self.__passenger_id += 1  # adds 1 to the defined passenger id
+        Passenger.__passenger_id += 1  # continuously adds 1 to each new instance of a new passenger
         self.destination = destination
 
-
-  # adds a way to add a passenger to the person
-    def add_passport(self,passportnum): # adds a way to add a passport to the person
+    # adds a way to add a passenger to the person
+    def add_passport(self, passportnum):  # adds a way to add a passport to the person
         self.__passport = passportnum
         return self.__passport
 
-
-    def check_passport(self): # Returns the passport number
+    def check_passport(self):  # Returns the passport number
         return self.__passport
-
-
-    def check_passenger(self): # returns the passenger id
-        return self.__passenger_id
-
-# Tests
-# person_1 = Passenger('0', 'Bob')
-# person_1.add_passport('312313')
-# person_1.add_passenger('12441')
-# print(person_1.check_passenger())
 
     def check_passenger(self):  # returns the passenger id
         return self.__passenger_id
 
-    def add_passenger_to_flight(self,flight_list):  #Goes through list of flights to add passenger
+    def check_passenger(self):  # returns the passenger id
+        return self.__passenger_id
+
+    def add_passenger_to_flight(self, flight_list):  # Goes through list of flights to add passenger
         for flight in flight_list:
             if self.destination == flight.destination:
                 flight.boarding_list.add_a_customer(self)
-
-#change
-
